4bc5f57fbd3569e6766a44a11742da6e25795d57
Rohit Khattar <rohitkh@byu,edu>
2018-08-17 11:47:47 +0545

    Updated Hydroviewer to have similar functions for lis and ecmwf and to consume lis data from an api.

--- tethysapp/hydroviewer_nepal/app.py
+++ tethysapp/hydroviewer_nepal/app.py
@@ -5,6 +5,7 @@ from tethys_sdk.permissions import Permission, PermissionGroup
 base_name = __package__.split('.')[-1]
 base_url = base_name.replace('_', '-')
 
+
 class Hydroviewer(TethysAppBase):
 
     name = 'HydroViewer {0}'.format(base_name.split('_')[-1].title())
@@ -29,11 +30,11 @@ class Hydroviewer(TethysAppBase):
             UrlMap(
                 name='ecmwf',
                 url='ecmwf-rapid',
-                controller='{0}.controllers.ecmwf'.format(base_name)),
+                controller='{0}.controllers.lis_ecmwf'.format(base_name)),
             UrlMap(
                 name='lis',
                 url='lis-rapid',
-                controller='{0}.controllers.lis'.format(base_name)),
+                controller='{0}.controllers.lis_ecmwf'.format(base_name)),
             UrlMap(
                 name='hiwat',
                 url='hiwat-rapid',
@@ -99,6 +100,10 @@ class Hydroviewer(TethysAppBase):
                 url='ecmwf-rapid/get-historic-data',
                 controller='{0}.controllers.get_historic_data'.format(base_name)),
             UrlMap(
+                name='get-historic-data',
+                url='lis-rapid/get-historic-data',
+                controller='{0}.controllers.get_historic_data'.format(base_name)),
+            UrlMap(
                 name='get-flow-duration-curve',
                 url='ecmwf-rapid/get-flow-duration-curve',
                 controller='{0}.controllers.get_flow_duration_curve'.format(base_name)),
@@ -158,7 +163,7 @@ class Hydroviewer(TethysAppBase):
                 name='forecastpercent',
                 url='forecastpercent',
                 controller='{0}.controllers.forecastpercent'.format(base_name)),
-        )
+            )
 
         return url_maps
 
@@ -186,78 +191,78 @@ class Hydroviewer(TethysAppBase):
                 type=CustomSetting.TYPE_STRING,
                 description='Tethys portal where Streamflow Prediction Tool is installed',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='spt_token',
                 type=CustomSetting.TYPE_STRING,
                 description='Unique token to access data from the Streamflow Prediction Tool',
                 required=True
-            ),
+                ),
+            CustomSetting(
+                name='sldas_source',
+                type=CustomSetting.TYPE_STRING,
+                description='Tethys portal where SLDAS Explorer is installed',
+                required=False
+                ),
+            CustomSetting(
+                name='sldas_token',
+                type=CustomSetting.TYPE_STRING,
+                description='Unique token to access data from the SLDAS Explorer',
+                required=False
+                ),
             CustomSetting(
                 name='geoserver',
                 type=CustomSetting.TYPE_STRING,
                 description='Spatial dataset service for app to use',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='workspace',
                 type=CustomSetting.TYPE_STRING,
                 description='Workspace within Geoserver where web service is',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='region',
                 type=CustomSetting.TYPE_STRING,
                 description='Streamflow Prediction Tool Region',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='keywords',
                 type=CustomSetting.TYPE_STRING,
                 description='Keyword(s) for visualizing watersheds in HydroViewer',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='zoom_info',
                 type=CustomSetting.TYPE_STRING,
                 description='lon,lat,zoom_level',
                 required=True
-            ),
+                ),
             CustomSetting(
                 name='extra_feature',
                 type=CustomSetting.TYPE_STRING,
                 description='Name of an additional feature to load from  the provided geoserver (e.g. a boundary layer).',
                 required=False,
-            ),
+                ),
             CustomSetting(
                 name='default_model_type',
                 type=CustomSetting.TYPE_STRING,
                 description='Default Model Type : (Options : ECMWF-RAPID, LIS-RAPID, HIWAT-RAPID)',
                 required=False
-            ),
+                ),
             CustomSetting(
                 name='default_watershed_name',
                 type=CustomSetting.TYPE_STRING,
                 description='Default Watershed Name: (e.g. "South America (Brazil)") ',
                 required=False
-            ),
+                ),
             CustomSetting(
                 name='show_dropdown',
                 type=CustomSetting.TYPE_BOOLEAN,
                 description='Hide Watershed Options when default present (True or False) ',
                 required=True,
                 value=False
-            ),
-            CustomSetting(
-                name='lis_path',
-                type=CustomSetting.TYPE_STRING,
-                description='Path to local LIS-RAPID directory',
-                required=False
-            ),
-            CustomSetting(
-                name='hiwat_path',
-                type=CustomSetting.TYPE_STRING,
-                description='Path to local HIWAT-RAPID directory',
-                required=False
-            ),
-        )
+                )
+            )
