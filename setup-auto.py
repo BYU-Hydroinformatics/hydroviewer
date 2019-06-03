744bbd8cbbfc48362ac3dc37fdcbbd8709a7249c
Rohit Khattar <rohitkh@byu,edu>
2019-06-03 15:29:57 -0600

    Added install.yml

--- setup-auto.py
+++ setup-auto.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from setuptools import setup, find_packages
-from tethys_apps.app_installation import custom_develop_command, custom_install_command
+from tethys_apps.app_installation import find_resource_files
 
 ### Apps Definition ###
 tethysapp_dir_list = [os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', child)
@@ -10,6 +10,11 @@ app_package = os.path.basename(filter(os.path.isdir, tethysapp_dir_list)[0])
 release_package = 'tethysapp-' + app_package
 app_class = '{0}.app:Hydroviewer'.format(app_package)
 app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
+
+# -- Get Resource File -- #
+resource_files = find_resource_files('tethysapp/' + app_package + '/templates')
+resource_files += find_resource_files('tethysapp/' + app_package + '/public')
+
 
 setup(
     name=release_package,
@@ -26,6 +31,5 @@ setup(
     namespace_packages=['tethysapp', 'tethysapp.' + app_package],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
-    cmdclass={}
+    install_requires=[]
 )
