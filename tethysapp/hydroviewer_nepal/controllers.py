33a7f7501593bee0a617c39550e6898922f6fdcc
Giovanni Romero Bustamante <ElkinGio@tmplab-32-120-122.et.byu.edu>
2019-07-22 13:09:07 -0600

    python3 updates

--- tethysapp/hydroviewer_nepal/controllers.py
+++ tethysapp/hydroviewer_nepal/controllers.py
@@ -140,7 +140,8 @@ def ecmwf(request):
 
     watershed_list = [['Select Watershed', '']] #+ watershed_list
     gurl=app.get_custom_setting('geoserver') + '/rest/workspaces/' + app.get_custom_setting('workspace') + '/featuretypes.json'
-    res2 = requests.get(gurl, auth=HTTPDigestAuth(app.get_custom_setting('user_geoserver'), app.get_custom_setting('password_geoserver')))
+    # res2 = requests.get(gurl, auth=HTTPDigestAuth(app.get_custom_setting('user_geoserver'), app.get_custom_setting('password_geoserver')))
+    res2 = requests.get(gurl, auth=HTTPBasicAuth(app.get_custom_setting('user_geoserver'), app.get_custom_setting('password_geoserver')),verify=False)
 
     for i in range(len(json.loads(res2.content)['featureTypes']['featureType'])):
         raw_feature = json.loads(res2.content)['featureTypes']['featureType'][i]['name']
