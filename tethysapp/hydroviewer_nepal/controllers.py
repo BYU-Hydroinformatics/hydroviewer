16da3d1581f96b1a61dec7edb33a33a88ef528bb
Giovanni Romero Bustamante <ElkinGio@tmplab-32-120-122.et.byu.edu>
2019-07-22 14:39:46 -0600

    python3 updates

--- tethysapp/hydroviewer_nepal/controllers.py
+++ tethysapp/hydroviewer_nepal/controllers.py
@@ -145,7 +145,7 @@ def ecmwf(request):
     # res2 = requests.get(gurl, auth=HTTPDigestAuth(app.get_custom_setting('user_geoserver'), app.get_custom_setting('password_geoserver')))
     # res2 = requests.get(gurl, auth=HTTPBasicAuth(app.get_custom_setting('user_geoserver'), app.get_custom_setting('password_geoserver')),verify=False)
     # res2 = requests.get(gurl, auth=HTTPBasicAuth(user, password),verify=False)
-    res2 = requests.get(gurl, auth=(user, password))
+    res2 = requests.get(gurl, auth=(user, password),verify=False)
 
 
 
