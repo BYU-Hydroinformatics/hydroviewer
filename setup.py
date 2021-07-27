51029ce46e574d89e4b84d0387114012740bb41b
Jorge Luis Sanchez <JorgeLuis@Civils-MacBook-Pro.local>
2021-07-27 16:45:07 -0600

    fix_tethys_init_files

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_namespace_packages
 from tethys_apps.app_installation import find_resource_files
-import fix_tethys_init_files
 
 # -- Apps Definition -- #
 app_package = 'hydroviewer_brazil'
@@ -13,8 +12,6 @@ dependencies = []
 resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
 resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
 resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
-
-fix_tethys_init_files.fix_tethys_init_files(3)
 
 setup(
     name=release_package,
